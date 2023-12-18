a38b513540753df4edf41dd3d1cc4ee2daa694e1
ed <pushpoped@gmail.com>
2023-12-17 22:59:28 -0600

    tracking status pages crud
    Signed-off-by: ed <pushpoped@gmail.com>

--- JMEliAppMaui/ViewModels/StatusPageViewModel.cs
+++ JMEliAppMaui/ViewModels/StatusPageViewModel.cs
@@ -12,15 +12,24 @@ namespace JMEliAppMaui.ViewModels
         #region props
         private bool  _isLoading,_isEdit,_isAdd,_isShow;
         private string _levelName, _selectedColor;
-        private string _SelectedDeepColor, _StatusDescription,_statusName;
+        private string _SelectedDeepColor, _SelectedStatusDescripsion, _StatusDescription,_statusName, _SelectedStatusName;
 
+
+        public string SelectedStatusDescripsion
+        {
+            get => _SelectedStatusDescripsion; set
+            {
+                _SelectedStatusDescripsion = value; OnPropertyChanged();
+            }
+        }
         public bool IsLoading { get => _isLoading; set { _isLoading = value; OnPropertyChanged(); } }
         public bool IsEdit { get => _isEdit; set { _isEdit = value; OnPropertyChanged(); } }
         public bool IsAdd { get => _isAdd; set { _isAdd = value; OnPropertyChanged(); } }
         public bool IsShow { get => _isShow; set { _isShow = value; OnPropertyChanged(); } }
 
         public string Title { get => _levelName; set { _levelName = value; OnPropertyChanged(); } }
-
+        
+        public string SelectedStatusName { get => _SelectedStatusName; set { _SelectedStatusName = value; OnPropertyChanged(); } }
         public string StatusName { get => _statusName; set { _statusName = value; OnPropertyChanged(); } }
         public string StatusDescription { get => _StatusDescription; set { _StatusDescription = value; OnPropertyChanged(); } }
 
@@ -36,7 +45,7 @@ namespace JMEliAppMaui.ViewModels
         public ICommand DeleteStatusCommand { get; private set; }
         public ICommand AddStatusCommand { get; private set; }
         public ICommand BackCommand { get; private set; }
-        public ICommand EditStatusCommand { get; private set; }
+        public ICommand DetailsStatusCommand { get; private set; }
         public ICommand SetSelectedColorCommand { get; private set; }
 
         #endregion
@@ -48,7 +57,7 @@ namespace JMEliAppMaui.ViewModels
               DeleteStatusCommand = new Command(OnDeleteStatusCommand);
               AddStatusCommand = new Command(OnAddStatusCommand);
               BackCommand = new Command(OnBackCommand);
-              EditStatusCommand = new Command(OnEditStatusCommand);
+              DetailsStatusCommand = new Command<StatusModel>(OnEditStatusCommand);
              SetSelectedColorCommand = new Command<string>(OnSetSelectedColorCommand);
             IsShow = true;
             IsEdit = false;
@@ -74,9 +83,20 @@ namespace JMEliAppMaui.ViewModels
             }
         }
 
-        private void OnEditStatusCommand(object obj)
+        private void OnEditStatusCommand(StatusModel obj)
         {
-            throw new NotImplementedException();
+            if (obj!=null)
+            {
+                SelectedStatusName = obj.Name;
+                SelectedStatusDescripsion = obj.Descripsion;
+                SelectedColor = obj.Color;
+                IsEdit = true;
+                IsAdd = false;
+                IsShow = false;
+
+            }
+
+
         }
 
         public void OnSetSelectedColorCommand(string obj)
@@ -114,6 +134,8 @@ namespace JMEliAppMaui.ViewModels
 
         public async void OnAddStatusCommand(object obj)
         {
+            IsLoading = true;
+
             if (IsAdd)
             {
                 if (string.IsNullOrEmpty(StatusDescription) ||
@@ -125,6 +147,7 @@ namespace JMEliAppMaui.ViewModels
                 }
                 else
                 {
+
                     var modelStatus = new StatusModel()
                     {
                         Color = SelectedColor,
@@ -132,12 +155,23 @@ namespace JMEliAppMaui.ViewModels
                         Descripsion = StatusDescription
                     };
 
-                   var id = await _fibAddGenericService.AddChild(modelStatus, "Status");
-
-                    if (!string.IsNullOrEmpty(id.ToString()))
+                    try
                     {
-                        modelStatus.Id = id.;
-                        await _fibAddGenericService.UpdateChild(modelStatus, "Status", id.ToString());
+                        var id = await _fibAddGenericService.AddChild(modelStatus, "Status");
+
+                        if (!string.IsNullOrEmpty(id.ToString()))
+                        {
+                            modelStatus.Id = id.ToString();
+                            await _fibAddGenericService.UpdateChild(modelStatus, "Status", id.ToString());
+
+                            await App.Current.MainPage.DisplayAlert("Success", $"Status {StatusName} added", "ok");
+                            OnBackCommand(null);
+                        }
+                    
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{ex.Message} {ex.Data}");
                     }
                 }
             }
@@ -146,6 +180,8 @@ namespace JMEliAppMaui.ViewModels
                 IsEdit = false;
                 IsShow = false;
             }
+
+            IsLoading = false;
         }
     }
 }
